6a6a9bbd4ab6580963398bf1b7c1ac61c9bddf1b
Chris <chrissx@chrissx.de>
2020-09-01 22:47:17 +0200

    made a lot of the c++ code just nicer

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,7 @@
-#include <cstddef>
 #include <cstdint>
+#include "../config.h"
 
-#define RASPI_TARGET 3
-
-// this is an awful hack to stop the compiler from bloating the binary
-namespace {
-        volatile uint32_t &mem(const uint32_t loc) {
-                return *reinterpret_cast<uint32_t *>(loc);
-        }
-        constexpr uint32_t bit(uint32_t i) {
-                return 1 << i;
-        }
-        void delay(int32_t count) {
-	        asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n" : "=r"(count): [count]"0"(count) : "cc");
-        }
-}
+#define spinwhile(b) while(b) asm volatile("nop")
 
 enum {
 #if RASPI_TARGET == 3
@@ -22,13 +9,16 @@ enum {
 #elif RASPI_TARGET == 4
         MMIO_BASE    = 0xFE000000,
 #else
-#error "No, you don't want to use non RasPi 3/4 ones."
+#error "Get yourself a RasPi 3/4!"
 #endif
 
         GPIO_BASE    = (MMIO_BASE + 0x200000),
         GPPUD        = (GPIO_BASE + 0x94),
         GPPUDCLK0    = (GPIO_BASE + 0x98),
-        UART0_BASE   = (GPIO_BASE + 0x1000),
+        // appended a 0, because maybe the addr was wrong and crap
+        UART0_BASE   = (GPIO_BASE + 0x10000),
+
+        // UART registers
         UART0_DR     = (UART0_BASE + 0x00),
         UART0_RSRECR = (UART0_BASE + 0x04),
         UART0_FR     = (UART0_BASE + 0x18),
@@ -47,14 +37,35 @@ enum {
         UART0_ITIP   = (UART0_BASE + 0x84),
         UART0_ITOP   = (UART0_BASE + 0x88),
         UART0_TDR    = (UART0_BASE + 0x8C),
+
+        // MBOX registers
         MBOX_BASE    = (MMIO_BASE + 0xB880),
         MBOX_READ    = (MBOX_BASE + 0x00),
         MBOX_STATUS  = (MBOX_BASE + 0x18),
         MBOX_WRITE   = (MBOX_BASE + 0x20)
 };
 
-volatile unsigned int  __attribute__((aligned(16))) mbox[9] = {
-    9*4, 0, 0x38002, 12, 8, 2, 3000000, 0 ,0
+// this is an awful hack to stop the compiler from bloating the binary
+namespace {
+        volatile uint32_t &mem(const uintptr_t loc) {
+                return *reinterpret_cast<uint32_t *>(loc);
+        }
+        constexpr uint32_t bit(const int i) {
+                return 1 << i;
+        }
+        inline void mbox(volatile uint32_t *packet) {
+                uint32_t r = ((uintptr_t)(packet) & ~0xF) | 8;
+                spinwhile(mem(MBOX_STATUS) & 0x80000000);
+                mem(MBOX_WRITE) = r;
+                spinwhile((mem(MBOX_STATUS) & 0x40000000) || mem(MBOX_READ) != r);
+        }
+        inline void delay(int32_t count) {
+	        asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n" : "=r"(count): [count]"0"(count) : "cc");
+        }
+}
+
+volatile uint32_t  __attribute__((aligned(16))) uart_clock_packet[9] = {
+    9*4, 0, 0x38002, 12, 8, 2, 3000000, 0, 0
 };
 
 void uart_init() {
@@ -65,39 +76,47 @@ void uart_init() {
         delay(150);
         mem(GPPUDCLK0) = 0;
         mem(UART0_ICR) = 0x7FF;
-        uint32_t r = (((uint64_t)(&mbox) & ~0xF) | 8);
-        while(mem(MBOX_STATUS) & 0x80000000);
-        mem(MBOX_WRITE) = r;
-        while((mem(MBOX_STATUS) & 0x40000000) || mem(MBOX_READ) != r);
-	mem(UART0_IBRD) = 1;
-	mem(UART0_FBRD) = 40;
-	mem(UART0_LCRH) = bit(4) | bit(5) | bit(6);
-	mem(UART0_IMSC) = bit(1) | bit(4) | bit(5) | bit(6) |
-	                  bit(7) | bit(8) | bit(9) | bit(10);
-	mem(UART0_CR)   = bit(0) | bit(8) | bit(9);
+        mbox(uart_clock_packet);
+        mem(UART0_IBRD) = 1;
+        mem(UART0_FBRD) = 40;
+        mem(UART0_LCRH) = bit(4) | bit(5) | bit(6);
+        mem(UART0_IMSC) = bit(1) | bit(4) | bit(5) | bit(6) |
+                          bit(7) | bit(8) | bit(9) | bit(10);
+        mem(UART0_CR)   = bit(0) | bit(8) | bit(9);
 }
 
-void uart_putc(char c) {
-	while(mem(UART0_FR) & bit(5));
-	mem(UART0_DR) = c;
+inline void uart_putc(const int c) {
+        spinwhile(mem(UART0_FR) & bit(5));
+        mem(UART0_DR) = c;
 }
 
-char uart_getc() {
-    while(mem(UART0_FR) & bit(4));
-    return mem(UART0_DR);
+inline int uart_getc() {
+        spinwhile(mem(UART0_FR) & bit(4));
+        return mem(UART0_DR);
 }
 
-void uart_puts(const char *str) {
-	for (; *str != '\0'; str++) {
-                uart_putc(*str);
-        }
+inline void uart_puts(const char *str) {
+        while(*str != '\0')
+                uart_putc(*str++);
+        uart_putc('\n');
 }
 
-extern "C" void kernel_main(uint64_t dtb_ptr32,
-                            uint64_t x1,
-                            uint64_t x2,
-                            uint64_t x3) {
+inline void uart_gets(char *bfr) {
+        int i;
+        while((i = uart_getc()) != '\n')
+                *bfr++ = i;
+        *bfr = '\0';
+}
+
+extern "C" void kmain() {
         uart_init();
-        uart_puts("Hello, kernel World!\n");
-        while(1) uart_putc(uart_getc());
+        uart_puts("Hello, kernel World!");
+        while(1) {
+                // yes, this will lead to a buffer overflow, if anyone
+                // is crazy enough to send more than 4096 chars in one
+                // line
+                char bfr[4096];
+                uart_gets(bfr);
+                uart_puts(bfr);
+        }
 }
