fd9c408a9ac67aa2203769ccef8c40f05542e96d
Chris <chrissx@chrissx.de>
2020-09-23 23:25:33 +0200

    kalloc

---
+++ src/kalloc.h
@@ -0,0 +1,23 @@
+#include "stdint.h"
+#include "memset.h"
+#include "pages.h"
+
+namespace {
+        inline static void *kalloc(size_t size) {
+                return k_page_alloc(size);
+        }
+
+        inline static void kfree(void *ptr) {
+                k_page_free(ptr);
+        }
+
+        inline static void *kcalloc(size_t nmemb, size_t size) {
+                size_t len = nmemb * size;
+                void *ptr = kalloc(len);
+                if(ptr) memset(ptr, 0, len);
+                return ptr;
+        }
+
+        //void *realloc(void *ptr, size_t size);
+        //void *reallocarray(void *ptr, size_t nmemb, size_t size);
+}
--- src/kinclude.h
+++ src/kinclude.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "stdint.h"
+#include "kalloc.h"
 #include "klib.h"
 #include "raspi/raspi.h"
 #include "printf.h"
